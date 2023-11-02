a7446c0d1ccb09ac8a1f621151e53ff2c44884af
ruframeru <zet.gundala.1210@gmail.com>
2023-11-02 11:14:51 +0700

    membuat koneksi ~fatur~

---
+++ src/koneksi/koneksi.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author utama
+ */
+public class koneksi {
+    private static Connection koneksi;
+    
+    public static Connection getKoneksi(){
+        if (koneksi == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/db_penjualan_barang_pas_xiia";
+                String user = "root";
+                String password = "";
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Berhasil");
+            } catch (SQLException e) {
+                System.out.println("Error");
+            }
+        }
+        return koneksi;
+    }
+    public static void main(String args[]){
+        getKoneksi();
+    }  
+}
