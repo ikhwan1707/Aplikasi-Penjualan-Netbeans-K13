c27c577fb0d00a81d0b7758ce3c1eaf30d02d6ab
syariefhasan05 <kcam01030508@gmail.com>
2023-11-02 14:10:19 +0700

    membuat create form distributor barang

--- src/koneksi/koneksi.java
+++ src/koneksi/koneksi.java
@@ -1,34 +1,42 @@
+package koneksi;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package koneksi;
+
 import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+
 /**
  *
- * @author utama
+ * @author smkiupro1
  */
 public class koneksi {
-    private static Connection koneksi;
     
-    public static Connection getkoneksi(){
-        if (koneksi == null) {
-            try {
-                String url ="jdbc:mysql://localhost:3306/db_penjualan_barang_pas_xiib?";
+    public static Connection koneksi;
+//    public static void main (String[] args){
+    public static Connection getKoneksi(){
+        if (koneksi == null)
+        {
+            try{
+                String url = "jdbc:mysql://localhost:3306/db_penjualan_barang_pas_xiib";
                 String user = "root";
                 String password = "";
                 DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
                 koneksi = DriverManager.getConnection(url, user, password);
-                System.out.println("Berhasil");
-            } catch (SQLException e) {
-                System.out.println("Error");
-            }
+                System.out.print("Berhasil Membuat Koneksi");
+            } catch (SQLException t) {
+                System.out.println("Erorr Membuat Koneksi");
+           }
         }
         return koneksi;
-    }
+     }
     public static void main(String args[]){
-        getkoneksi();
-    }  
+        getKoneksi();
+    }
 }
